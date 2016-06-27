355c9ab75e7bb989f8b2cc7edaea63537eef517a
Venkataanish <sistla@usc.edu>
2016-06-27 16:43:44 -0700

    Unicast sender and receiver - Setting Txpower

--- examples/rime/example-unicast.c
+++ examples/rime/example-unicast.c
@@ -39,20 +39,51 @@
 
 #include "contiki.h"
 #include "net/rime/rime.h"
+#include "net/rime/rime.h"
 #include "random.h"
 #include "dev/button-sensor.h"
 #include "dev/leds.h"
 #include "cc2420.h"
 #include "sys/node-id.h"
 
+#include <stdio.h>
 
-#include <stdio.h> /* For printf() */
+#include "contiki-lib.h"
+#include "contiki-net.h"
 
+#include "mac.h"
+
+#include <string.h>
+
+#define MACDEBUG 0
+
+#define DEBUG 0
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF(" %02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x ", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF(" %02x:%02x:%02x:%02x:%02x:%02x ",lladdr->addr[0], lladdr->addr[1], lladdr->addr[2], lladdr->addr[3],lladdr->addr[4], lladdr->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#endif
+
+#define PING6_NB 5
+#define PING6_DATALEN 16
+
+#define UIP_IP_BUF                ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
+#define UIP_ICMP_BUF            ((struct uip_icmp_hdr *)&uip_buf[uip_l2_l3_hdr_len])
+
+
+
+
+/*uint8_t nodid=4;*/
 #if WITH_TINYOS_AUTO_IDS
-uint16_t TOS_NODE_ID = 0x1234; /* non-zero */
-uint16_t TOS_LOCAL_ADDRESS = 0x1234; /* non-zero */
+  uint8_t node_id = 0x1234;
+  node_id_restore();
 #endif /* WITH_TINYOS_AUTO_IDS */
-/*---------------------------------------------------------------------------*/
+/*node_id_burn(nodid);*/
+
 unsigned long
 strtolong(const char *str, const char **retstr)
 {
@@ -81,7 +112,6 @@ strtolong(const char *str, const char **retstr)
   
   return num;
 }
-
 /*---------------------------------------------------------------------------*/
 PROCESS(example_unicast_process, "Example unicast");
 AUTOSTART_PROCESSES(&example_unicast_process);
@@ -89,11 +119,17 @@ AUTOSTART_PROCESSES(&example_unicast_process);
 static void
 recv_uc(struct unicast_conn *c, const linkaddr_t *from)
 {
-  printf("\nReceived power : %s\n",(char *)packetbuf_dataptr());
+  printf("unicast message received from node %d\n",
+	 from->u8[0]);
+  printf("\nReceived power %s\n",(char *)packetbuf_dataptr());
   const char *numb = (char *)packetbuf_dataptr();
   uint16_t txnum = strtolong(numb, &numb);
+  //uint8_t txnum  =(uint8_t *)packetbuf_dataptr();
   cc2420_set_txpower (txnum);
-  printf("Tx power read from register & is set to %u\n",cc2420_get_txpower());
+  printf("Node id is : %u\n",node_id);
+  printf("Tx power read from register and set to %u\n",cc2420_get_txpower());
+
+
 }
 /*---------------------------------------------------------------------------*/
 static void
@@ -103,8 +139,9 @@ sent_uc(struct unicast_conn *c, int status, int num_tx)
   if(linkaddr_cmp(dest, &linkaddr_null)) {
     return;
   }
-  printf("unicast message sent to node(with id) %d\n",
-    dest->u8[0]);
+  printf("unicast message sent to node %d\n",
+    dest->u8[0], status, num_tx);
+  printf("Node id is %u\n",node_id);
 }
 /*---------------------------------------------------------------------------*/
 static const struct unicast_callbacks unicast_callbacks = {recv_uc, sent_uc};
@@ -115,26 +152,32 @@ PROCESS_THREAD(example_unicast_process, ev, data)
   PROCESS_EXITHANDLER(unicast_close(&uc);)
     
   PROCESS_BEGIN();
+  char *txpwr;
+  
+  //printf("Enter a Tx power between 0-31\n");
+  //scanf("%s", txpwr);
 
   unicast_open(&uc, 146, &unicast_callbacks);
 
-  while(1) {
+  //while(1) {
     static struct etimer et;
     linkaddr_t addr;
     
     etimer_set(&et, CLOCK_SECOND);
     
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-
+    
     packetbuf_copyfrom("11", 3);
     addr.u8[0] = 1;
     addr.u8[1] = 0;
     if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
       unicast_send(&uc, &addr);
-      printf("\n Sending transmission to be set to 11");
+    printf("Sending Txpower to be set as 11\n");
+    
+    printf("\n");
     }
 
-  }
+  //}
 
   PROCESS_END();
 }
